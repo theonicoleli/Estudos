9c5c8ff1eb8d9c45084a81a77ae34f9e6260cfdc
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-05-02 12:17:31 -0300

    Estudos sobre Tupla

--- Curso Python/aula14.py
+++ Curso Python/aula14.py
@@ -1,12 +1,15 @@
-# Estudos de Tupla
+print('Estudos de Tupla')
 contagem = ('Ricardo', 'Cezar', 'Fabrício', 'João', 'Pedro')
-#contagem = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+#numero = int(input('Digite um número: '))
+#contagem = ('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
 #for cont in contagem:
 #    print(f'O valor atribuido a esta posição é de {cont}')
 #for cont in range(0, len(contagem)):
     #print(f'O  valor atribuido a esta posição é de {cont}')
 #for cont in range(0, len(contagem)):
     #print(f'O valor atribuido a esta posição é de {contagem[cont]}')
+#for cont in range(1):
+#    print(f'Seu número escrito é {contagem[numero]}!')
 insercao = int(input('Digite um número(máx 5): '))
 for cont in range(1):
     print(f'Seu número está na posição de: {contagem[insercao-1]}')
