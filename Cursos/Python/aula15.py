315e58503b9579eb85d00ea1761bb5aa81add58d
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-05-05 21:18:15 -0300

    Python & HTML

--- Cursos/Python/aula15.py
+++ Cursos/Python/aula15.py
@@ -1,11 +1,11 @@
 list = [33,37,44,16,21,1]
-lista_dois = [3,4,5,6,7]
+lista_dois = [8,4,5,6,7]
 list[2] = 44 # Posição da lista e seu valor
-list.append(12) # Adicioonando o número 12 na lista
+list.append(12) # Adicionando o número 12 na lista
 print(list)
-list.sort() # Formata a lista para que fique organizada do maior ao menor
+list.sort() # Formata a lista para que fique organizada do menor para o maior
 print(list)
-list.insert(27, 3)
+list.insert(3, 27) # Posição e elemento
 list.remove(1) # Removendo o número 1 da lista
 list.pop() # Removendo o último número da lista
 list.pop(0) # Removendo o número da posição 0
