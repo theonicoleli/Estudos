492d26a77ebf02db9649eac42c77173d6c75b822
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-05-09 15:41:02 -0300

    Python & HTML

--- Cursos/Python/aula05.py
+++ Cursos/Python/aula05.py
@@ -1,8 +1,33 @@
-print('Eleição dos alunos')
+# print('Eleição dos alunos')
 augusto=0
 davy=0
-quantidade_alunos = int(input("Digite quantos alunos a sala de aula tem: "))
-while quantidade_alunos>0:
+# quantidade_alunos = int(input("Digite quantos alunos a sala de aula tem: "))
+# while quantidade_alunos>0:
+#     print('Escolha o representante: ')
+#     print('Augusto(1)')
+#     print('Davy(2)')
+#     voto = int(input('Digite ao lado o representante: '))
+#     if voto==1:
+#         augusto+=1
+#         print('Você votou em Augusto!')
+#         quantidade_alunos-=1
+#     elif voto==2:
+#         davy+=1
+#         print('Você votou em Davy')
+#         quantidade_alunos-=1
+#     else:
+#         print('Escolha um número válido')
+# if quantidade_alunos==0:
+#     if augusto>davy:
+#         print('Augusto é o representante!')
+#     elif davy>augusto:
+#         print('Davy é o representante!')
+#     else:
+#         print('Deve ser realizado uma nova eleição, pois os votos foram igualados!')
+
+print('Eleição dos alunos')
+quantidade_alunos = int(input('Digite quantos alunos a sala de aula tem: '))
+for cont in range(quantidade_alunos):
     print('Escolha o representante: ')
     print('Augusto(1)')
     print('Davy(2)')
@@ -10,17 +35,16 @@ while quantidade_alunos>0:
     if voto==1:
         augusto+=1
         print('Você votou em Augusto!')
-        quantidade_alunos-=1
     elif voto==2:
         davy+=1
         print('Você votou em Davy')
         quantidade_alunos-=1
     else:
         print('Escolha um número válido')
-if quantidade_alunos==0:
-    if augusto>davy:
-        print('Augusto é o representante!')
-    elif davy>augusto:
-        print('Davy é o representante!')
-    else:
-        print('Deve ser realizado uma nova eleição, pois os votos foram igualados!')
+print('='*100)
+if augusto>davy:
+    print(f'Augusto é o representante com {augusto} votos! Davy ficou com {davy} votos')
+elif davy>augusto:
+    print(f'Davy é o representante com {davy} votos! Augusto ficou com {augusto} votos')
+else:
+    print(f'Deve ser realizado uma nova eleição, pois os votos foram igualados! Davy ficou com {davy} votos, já Augusto ficou com {augusto} votos!')
