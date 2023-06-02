0b695c12e18bad5548c939e73e98813e8cea6c38
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-06-02 11:53:42 -0300

    Python & HTML

--- Cursos/Python/Trabalhos/Maquina_Refri_2.py
+++ Cursos/Python/Trabalhos/Maquina_Refri_2.py
@@ -6,13 +6,6 @@ continuando_compra = []
 produtos_preco_quantidade = []
 telefone_pix = []
 
-entrada_moedas_1_real = 0
-entrada_moedas_50_cent = 0
-entrada_moedas_25_cent = 0
-entrada_cedulas_2_reais = 0
-entrada_cedulas_5_reais = 0
-entrada_cedulas_10_reais = 0
-entrada_cedulas_20_reais = 0
 moedas_1_real_pagamento = 0
 moedas_50_cent_pagamento = 0
 moedas_25_cent_pagamento = 0
@@ -42,49 +35,6 @@ cedula_20_reais = 0
 moedas_totais = moedas_50_cent+moedas_25_cent+moedas_1_real+moedas_1_real_pagamento+moedas_25_cent_pagamento+moedas_50_cent_pagamento
 cedulas_totais = cedula_2_reais+cedula_5_reais+cedula_10_reais+cedula_20_reais+cedulas_20_reais_pagamento+cedulas_10_reais_pagamento+cedulas_5_reais_pagamento+cedulas_2_reais_pagamento
 cedulas_moedas_totais = moedas_50_cent+moedas_25_cent+moedas_1_real+moedas_1_real_pagamento+moedas_25_cent_pagamento+moedas_50_cent_pagamento+cedula_2_reais+cedula_5_reais+cedula_10_reais+cedula_20_reais
-
-def pagamento_final(troco):
-    moedas_pagamento_total = moedas_1_real_pagamento+moedas_25_cent_pagamento+moedas_50_cent_pagamento
-    cedulas_pagamento_total = cedulas_20_reais_pagamento+cedulas_10_reais_pagamento+cedulas_5_reais_pagamento+cedulas_2_reais_pagamento
-    moedas_1_real = 0 + moedas_1_real_pagamento
-    moedas_50_cent = 0 + moedas_50_cent_pagamento
-    moedas_25_cent = 0 + moedas_25_cent_pagamento
-    cedula_20_reais = 0 + cedulas_20_reais_pagamento
-    cedula_10_reais = 0 + cedulas_10_reais_pagamento
-    cedula_5_reais = 0 + cedulas_5_reais_pagamento
-    cedula_2_reais = 0 + cedulas_2_reais_pagamento
-    print(f'Seu troco foi de {troco}')
-    if moedas_pagamento_total>0:
-        while troco>=20 and cedula_20_reais>0:
-            print("Estamos mandando uma cédula de 20 reais")
-            troco -= 20
-            cedulas_20_reais-=1
-        while troco>= 10 and cedula_10_reais>0:
-            print("Estamos mandando uma cédula de 10 reais")
-            troco -= 10
-            cedula_10_reais-=1
-        while troco>= 5 and cedula_5_reais>0:
-            print("Estamos mandando uma cédula de 5 reais")
-            troco -= 5
-            cedula_5_reais-=1
-        while troco>= 2 and cedula_2_reais>0:
-            print("Estamos mandando uma cédula de 2 reais")
-            troco -= 2
-            cedula_2_reais-=1
-        while troco >= 1 and moedas_1_real>0:
-            print("Enviando uma moeda de 1 real")
-            troco -= 1
-            moedas_1_real-=1
-        while troco >= 0.5 and moedas_50_cent>0:
-            print("Estamos mandando uma moeda de 50 centavos")
-            troco -= 0.5
-            moedas_50_cent-=1
-        while troco >= 0.25 and moedas_25_cent>0:
-            print("Estamos mandando uma moeda de 25 centavos")
-            troco -= 0.25
-            moedas_25_cent-=1
-        if troco <= 0.04:
-            troco = 0
 
 nomeadm = str(input("Olá Administrador, digite seu nome: "))
 nomemaquina = str(input("Digite o nome ao qual você deseja que a máquina tenha: "))
@@ -127,6 +77,13 @@ while True:
     if fim == 1:
         for item in continuando_compra:
             item[1] = 0
+    entrada_moedas_1_real = 0
+    entrada_moedas_50_cent = 0
+    entrada_moedas_25_cent = 0
+    entrada_cedulas_2_reais = 0
+    entrada_cedulas_5_reais = 0
+    entrada_cedulas_10_reais = 0
+    entrada_cedulas_20_reais = 0
     fim = 0
     preco_dos_produtos_atual = 0
     entrada_moedas_25_cent = 0
@@ -134,6 +91,7 @@ while True:
     entrada_moedas_1_real = 0
     continuacao_while = 0
     continuar_pagando = 0
+    pagamento_total = 0
     print('-='*30)
     print('Insira qual bebida você deseja comprar:')
     print('-='*30)
@@ -142,7 +100,10 @@ while True:
     selecao = int(input('Digite o número do refri para a compra: '))
     if 1 <= selecao <= quant_refri:
         quantidade_selecionada = int(input('Digite a quantidade desejada para comprar: '))
-        if quantidade_dos_produtos[selecao-1] > quantidade_selecionada:
+        if quantidade_selecionada == 0:
+            print('Digite uma quantidade válida!')
+            continue
+        elif quantidade_dos_produtos[selecao-1] > quantidade_selecionada:
             encontrado = False
             for item in continuando_compra:
                 if item[0] == lista_de_produtos[selecao-1]:
@@ -231,7 +192,7 @@ while True:
                     pagamento_total = (entrada_moedas_25_cent*0.25)+(entrada_moedas_50_cent*0.5)+(entrada_moedas_1_real*1)+(entrada_cedulas_2_reais*2)+(entrada_cedulas_5_reais*5)+(entrada_cedulas_10_reais*10)+(entrada_cedulas_20_reais*20)
                 elif pagamento == 4:
                     pagamento_pix = 0
-                    telefone = int(input('Digite o número de seu telefone: '))
+                    telefone = int(input('Digite o número de seu telefone (9 digitos): '))
                     telefone_str = str(telefone)
                     if len(telefone_str) == 9:
                         print('Nossa chave pix é 987.349.859-76')
@@ -243,6 +204,7 @@ while True:
                                 print('Muito obrigado por comprar conósco!')
                                 pagamento_pix = preco_dos_produtos_atual
                                 telefone_pix.append([telefone, pagamento_pix])
+                                quantidade_dos_produtos[selecao-1] -= quantidade_selecionada
                                 fim = 1
                                 continuacao_while = 1
                                 break
@@ -287,6 +249,8 @@ while True:
                     quantidade_dos_produtos[selecao-1] -= quantidade_selecionada
                     if pagamento_total == preco_dos_produtos_atual:
                         print(f'Muito obrigado por comprar conosco!\nA quantidade atual de {lista_de_produtos[selecao-1]} é de {quantidade_dos_produtos[selecao-1]}')
+                        fim = 1
+                        break
                     elif pagamento_total > preco_dos_produtos_atual:
                         print(f'Muito obrigado por comprar conosco!\nA quantidade atual desta bebida é de {quantidade_dos_produtos[selecao-1]}')
                         if pagamento_continuacao==0:
@@ -296,10 +260,76 @@ while True:
                         if pagamento_total == 0:
                             break
                         else:
-                            pagamento_final(pagamento_total)
-                        continuacao_while==1
-                    fim = 1
-                    break
+                            moedas_pagamento_total = moedas_1_real_pagamento+moedas_25_cent_pagamento+moedas_50_cent_pagamento
+                            cedulas_pagamento_total = cedulas_20_reais_pagamento+cedulas_10_reais_pagamento+cedulas_5_reais_pagamento+cedulas_2_reais_pagamento
+                            moedas_1_real = 0 + moedas_1_real_pagamento
+                            moedas_50_cent = 0 + moedas_50_cent_pagamento
+                            moedas_25_cent = 0 + moedas_25_cent_pagamento
+                            cedula_20_reais = 0 + cedulas_20_reais_pagamento
+                            cedula_10_reais = 0 + cedulas_10_reais_pagamento
+                            cedula_5_reais = 0 + cedulas_5_reais_pagamento
+                            cedula_2_reais = 0 + cedulas_2_reais_pagamento
+                            moedas_cedulas_pagamento = moedas_1_real_pagamento+moedas_25_cent_pagamento+moedas_50_cent_pagamento+cedulas_20_reais_pagamento+cedulas_10_reais_pagamento+cedulas_5_reais_pagamento+cedulas_2_reais_pagamento
+                            print(f'Seu troco foi de {pagamento_total}')
+                            if moedas_cedulas_pagamento>0:
+                                while pagamento_total>=20 and cedula_20_reais>0:
+                                    print("Estamos mandando uma cédula de 20 reais")
+                                    pagamento_total -= 20
+                                    cedula_20_reais-=1
+                                while pagamento_total>= 10 and cedula_10_reais>0:
+                                    print("Estamos mandando uma cédula de 10 reais")
+                                    pagamento_total -= 10
+                                    cedula_10_reais-=1
+                                while pagamento_total>= 5 and cedula_5_reais>0:
+                                    print("Estamos mandando uma cédula de 5 reais")
+                                    pagamento_total -= 5
+                                    cedula_5_reais-=1
+                                while pagamento_total>= 2 and cedula_2_reais>0:
+                                    print("Estamos mandando uma cédula de 2 reais")
+                                    pagamento_total -= 2
+                                    cedula_2_reais-=1
+                                while pagamento_total >= 1 and moedas_1_real>0:
+                                    print("Enviando uma moeda de 1 real")
+                                    pagamento_total -= 1
+                                    moedas_1_real-=1
+                                while pagamento_total >= 0.5 and moedas_50_cent>0:
+                                    print("Estamos mandando uma moeda de 50 centavos")
+                                    pagamento_total -= 0.5
+                                    moedas_50_cent-=1
+                                while pagamento_total >= 0.25 and moedas_25_cent>0:
+                                    print("Estamos mandando uma moeda de 25 centavos")
+                                    pagamento_total -= 0.25
+                                    moedas_25_cent-=1
+                                if pagamento_total <= 0.04:
+                                    pagamento_total = 0
+                                continuacao_while==1
+                                fim = 1
+                                break
+                            else:
+                                print('Estamos sem troco, enviaremos seu dinheiro!')
+                                if entrada_moedas_25_cent>0:
+                                    print(f'Enviando as moedas de 25 centavos = [{entrada_moedas_25_cent}]')
+                                    entrada_moedas_25_cent -= entrada_moedas_25_cent
+                                if entrada_moedas_50_cent>0:
+                                    print(f'Enviando as moedas de 50 centavos = [{entrada_moedas_50_cent}]')
+                                    entrada_moedas_50_cent -= entrada_moedas_50_cent   
+                                if entrada_moedas_1_real>0:                             
+                                    print(f'Enviando as moedas de 1 real = [{entrada_moedas_1_real}]')
+                                    entrada_moedas_1_real -= entrada_moedas_1_real
+                                if entrada_cedulas_2_reais>0:
+                                    print(f'Enviando as cédulas de 2 reais = [{entrada_cedulas_2_reais}]')
+                                    entrada_cedulas_2_reais -= entrada_cedulas_2_reais
+                                if entrada_cedulas_5_reais:
+                                    print(f'Enviando as cédulas de 5 reais = [{entrada_cedulas_5_reais}]')
+                                    entrada_cedulas_5_reais -= entrada_cedulas_5_reais
+                                if entrada_cedulas_10_reais:
+                                    print(f'Enviando as cédulas de 10 reais = [{entrada_cedulas_10_reais}]')
+                                    entrada_cedulas_10_reais -= entrada_cedulas_10_reais
+                                if entrada_cedulas_20_reais:
+                                    print(f'Enviando as cédulas de 20 reais = [{entrada_cedulas_20_reais}]')
+                                    entrada_cedulas_20_reais -= entrada_cedulas_20_reais
+                                fim = 1
+                                break
             else:
                 print('Selecione um valor válido')
         else:
@@ -311,10 +341,10 @@ while True:
             print(f"[{bebidas+1}]", lista_de_produtos[bebidas],", quantidade = ", quantidade_dos_produtos[bebidas])
         print('Ou digite [0] caso não queira acrescentar nenhuma bebida')
         escolha = int(input('Digite qual bebida você deseja acrescentar: '))
-        if 1 <= escolha <= len(lista_de_produtos) and escolha != 0 :
+        if 1 <= escolha <= len(lista_de_produtos) and escolha != 0:
             nova_quantidade = int(input('Digite quantas novas unidades você deseja acrescentar: '))
-            quantidade_dos_produtos[escolha] += nova_quantidade
-            print(f'A nova quantidade de {lista_de_produtos[escolha]} é de: {quantidade_dos_produtos[escolha]}')
+            quantidade_dos_produtos[escolha - 1] += nova_quantidade
+            print(f'A nova quantidade de {lista_de_produtos[escolha - 1]} é de: {quantidade_dos_produtos[escolha - 1]}')
         elif escolha == 0:
             menu_adm=int(input("OK, gostaria de realizar outra operação?\n[1] Ver saldo total de produtos\n[2] Ver lucro de todos os produtos\n[3] Adicionar troco\n[4] Consultar cachê\n[5] Alterar produtos e valores\n[6] Movimentações de PIX\n[7] Cancelar operação\nDigite sua escolha ao lado: "))
             if menu_adm == 1:
@@ -363,14 +393,17 @@ while True:
                     quantidade_dos_produtos[escolha_troca-1] = int(input(f'Digite a quantidade para o/a {lista_de_produtos[escolha_troca-1]}: '))
                     preco_dos_produtos[escolha_troca-1] = float(input(f'Digite o preço do/da {lista_de_produtos[escolha_troca-1]}: '))
                     preco_dos_produtos[escolha_troca-1] = round(preco_dos_produtos[escolha_troca-1], 2)
+                    produtos_preco_quantidade[escolha_troca-1][0] = lista_de_produtos[escolha_troca-1]
+                    produtos_preco_quantidade[escolha_troca-1][1] = preco_dos_produtos[escolha_troca-1]
+                    produtos_preco_quantidade[escolha_troca-1][2] = quantidade_dos_produtos[escolha_troca-1]
                 else:
                     print('Escolha uma bebida válida!')
             elif menu_adm == 6:
                 recibo_pix_total = 0
                 for tp in telefone_pix:
-                    print(f'O telefone {tp[0]} realizou um pagamento de {tp[1]}')
+                    print(f'O telefone {tp[0]} realizou um pagamento de: R$ {tp[1]}')
                     recibo_pix_total += tp[1]
-                print(f'O pagamento total realizado foi de {recibo_pix_total}')
+                print(f'O pagamento total realizado foi de: R$ {recibo_pix_total}')
             elif menu_adm == 7:
                 print('Ok! Tenha um excelente dia administrador!')
     else:
