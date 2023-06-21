27e773e24193af3ccf1bd4ec14b1ea1d5aaccb50
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-06-21 15:30:42 -0300

    Python & HTML

---
+++ Cursos/Python/aula49.py
@@ -0,0 +1,13 @@
+frutas = ['laranja', 'maça', 'pera', 'banana', 'maça', 'banana']
+numeros = [1, 2,3]
+varios = [10, 3.5, 'texto']
+
+lista = []
+
+print(frutas[0])
+indice = 1
+print(frutas[indice])
+for cont in range(frutas.__len__()):
+    print(frutas[cont])
+for cont in range(len(frutas)):
+    print(frutas[cont])
