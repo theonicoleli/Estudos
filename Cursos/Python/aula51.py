ec55ce7c9c6709f6bfc8ad62b449cf283ef12af7
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-06-21 15:31:19 -0300

    Python & HTML

---
+++ Cursos/Python/aula51.py
@@ -0,0 +1,12 @@
+# nLinhas = 3
+# nColunas = 5
+# matriz_dinamica = [0] * nLinhas
+
+# for linha in range(nLinhas):
+#     matriz_dinamica[linha] = [0] * nColunas
+
+# for linha in range(nLinhas):
+#     for coluna in range(nColunas):
+#         matriz_dinamica[linha][coluna] = int(input(f'Digite o número para a linha {linha+1} e coluna {coluna+1}: '))
+
+# print(matriz_dinamica)
