82725b949b3ae4b7e7c20e7f346ff85c4811d963
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-07-03 20:42:52 -0300

    Starting JAVA

---
+++ Cursos/JAVA/STUDYING JAVA/estudo_1.java
@@ -0,0 +1,8 @@
+public class meu_primeiro_documento_java {
+
+    public static void(String[]args) throws Exception {
+
+        System.out.println("Hello World!")
+    }
+
+}
---
+++ Cursos/JAVA/src/main/java/com/example/aulaclass1/Aulaclass1Application.java
@@ -0,0 +1,13 @@
+package com.example.aulaclass1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aulaclass1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aulaclass1Application.class, args);
+	}
+
+}
