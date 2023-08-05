96c67b356be9d4808d8c985d89fcc8cb7753a75d
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-08-04 22:03:33 -0300

    JavaScript
    Estudos com funções em JavaScript

---
+++ Cursos/JavaScript/studying_js/class4.js
@@ -0,0 +1,100 @@
+// Alguns exercícios:
+
+
+// 1)
+
+// function sum(a, b) {
+//     var somar = a + b;
+//     return somar;
+// }
+
+// console.log(sum(3,2));
+
+
+// 2)
+
+// function verifyThings(a, b) {
+//     if (a === b) {
+//         return true;
+//     } return false;
+// }
+
+// console.log(verifyThings('a', 'a'));
+// console.log(verifyThings(1, 1));
+// console.log(verifyThings(1, 2));
+
+
+// 3) 
+
+// function collection(a) {
+//     return typeof a;
+// }
+
+// console.log(collection('a'));
+// console.log(collection(2));
+// console.log(collection());
+
+
+// 4)
+
+// function charThings(a, n) {
+//     var charPosition = a[n];
+//     return charPosition;
+// }
+
+// console.log(charThings('natan', (4-1)));
+// console.log(charThings('Lima', (4-1)));
+
+
+// 5)
+
+// function slicingThings(a) {
+//     var fatiando = a.slice(1, 3);
+//     //var fatiando_other_way = a.substring(1, 3);
+//     return fatiando;
+// }
+
+// console.log(slicingThings('Lima'))
+
+
+// 6)
+
+// function lastThreeLetters(a) { // retornando os últimos 3 termos da string ou números
+//     var turningString = a.toString();   
+//     var fatiando = turningString.slice(-3);
+//     return fatiando;
+// }
+
+// console.log(lastThreeLetters('Ricardinho Pereira'))
+// console.log(lastThreeLetters(1231434235))
+
+
+// 7)
+
+// function firstThreeLetters(a) {
+//     var turningString = a.toString();
+//     var fatiando = turningString.slice(0, 3);
+//     return fatiando
+// }
+
+// console.log(firstThreeLetters(3124324))
+// console.log(firstThreeLetters('kfdsjhgsgh'))
+
+
+// 8)
+
+// function searchingA(a) {
+//     return a.indexOf('a') + 1;
+// }
+
+// console.log('Foi encontrado: ', searchingA('abajur'));
+// console.log('Foi encontrado: ',searchingA('josé'));
+
+
+//9)
+
+// function percentage(a, b) {
+//     return a * (b/100);
+// }
+
+// console.log(`A porcentagem entre ${100} e ${50}: `, percentage(100, 50));
