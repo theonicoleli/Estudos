37e7fb211c7f9079b1a68e3408b3620b38d35e07
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-08-28 17:16:03 -0300

    JAVA
    Estudos de ENUM com java

---
+++ Cursos/JAVA/Orientado a Objetos/ENUM Java/src/entitie/Order.java
@@ -0,0 +1,54 @@
+package entitie;
+
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+
+public class Order {
+    private Integer id;
+    private Date moment;
+    private OrderStatus status;
+
+    public Order() {
+
+    }
+
+    public Order(Integer id, Date moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", status=" + status +
+                '}';
+    }
+}
