4dbc176e319d84ba80686582feed6875a17cdbf1
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-10-15 19:49:25 -0300

    Java
    Finalizando estudos de Interface

---
+++ Cursos/JAVA/Orientado a Objetos/BrazilService/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import services.BrazilInterestService;
+import services.InterestService;
+import services.UsaInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
+
+        InterestService is = new BrazilInterestService(2.0);
+        double payment = is.payment(amount, months);
+
+        System.out.println("Payment after " + months + " months: ");
+        System.out.println(String.format("%.2f", payment));
+
+        sc.close();
+    }
+}
---
+++ Cursos/JAVA/Orientado a Objetos/InterfaceComparable/src/entities/Employee.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class Employee implements Comparable<Employee>{
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNome(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public int compareTo(Employee other) {
+        return name.compareTo(other.getName());
+    }
+}
