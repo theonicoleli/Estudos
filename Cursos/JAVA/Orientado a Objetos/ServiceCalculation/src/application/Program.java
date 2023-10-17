b0a2be1eac91f823a700fa1d2057e76f5ef87cfa
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2023-10-17 14:21:16 -0300

    Java
    Estudos de métodos estáticos genéricos

---
+++ Cursos/JAVA/Orientado a Objetos/ServiceCalculation/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import entities.Product;
+import services.CalculationService;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        String path = "C:\\temp\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.println("Most expensive: ");
+            System.out.println(x);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
